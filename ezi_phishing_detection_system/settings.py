c6781b7e004812276f512102531bc41426dd6094
eshamudasar <130305460+eshamudasar@users.noreply.github.com>
2024-12-17 00:05:10 +0500

    Update settings.py

--- ezi_phishing_detection_system/settings.py
+++ ezi_phishing_detection_system/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = '4s=#jl+2tskgp!!^7kosnk$+n=s43(s0o)q3pra&k&*c+gx4$1'  # Replace wit
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  # Set to False in production
 
-ALLOWED_HOSTS = ['localhost', '20.48.202.171', 'ezi-c5fcd8csh8c2bheb.canadacentral-01.azurewebsites.net']  # Allowed hosts for your app
+ALLOWED_HOSTS = ['localhost', 'ezi-c5fcd8csh8c2bheb.canadacentral-01.azurewebsites.net']  # Allowed hosts for your app
 
 # Application definition
 INSTALLED_APPS = [
